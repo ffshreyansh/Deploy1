8b0d41c94d4e4c227df66fbd6f099d34fe8d23d5
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2023-01-12 19:03:36 +0530

    added filter by Name

--- app.js
+++ app.js
@@ -48,92 +48,96 @@ MongoClient.connect(db, { useNewUrlParser: true }, function (err, client) {
 
     app.post('/', function (req, res) {
 
-        const name = req.body.name;
-        const number = req.body.number;
-        const data = number.split(',');
-
-        let num = [];
-        data.forEach(number => {
-            num.push(number + " - " + token++);
-        });
-
-        db.collection('users').findOne({ name: name }, function (err, result) {
-            if (err) {
-                console.log(err);
-            }
-            // If a document with the same name already exists, return an error
-            if (result) {
-                res.send("Data Already Exists with the Same Name!");
-            } else {
-
-                // Insert the data into the database
-                db.collection('users').insertOne({ name: name, number: num }, function (err, result) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    console.log('Data added to the collection');
-                    res.redirect('/success');
-                });
-            }
-        });
-
-    });
+      const name = req.body.name;
+      const number = req.body.number;
+      const data = number.split(',');
+  
+      let num = [];
+      data.forEach(number => {
+          num.push(number + " - " + token++);
+      });
+  
+      // Add the date field with the current date
+      const date = new Date();
+  
+      db.collection('users').findOne({ name: name }, function (err, result) {
+          if (err) {
+              console.log(err);
+          }
+          // If a document with the same name already exists, return an error
+          if (result) {
+              res.send("Data Already Exists with the Same Name!");
+          } else {
+  
+              // Insert the data into the database
+              db.collection('users').insertOne({ name: name, number: num, date: date }, function (err, result) {
+                  if (err) {
+                      console.log(err);
+                  }
+                  console.log('Data added to the collection');
+                  res.redirect('/success');
+              });
+          }
+      });
+  
+  });
+  
         //Using Puppeteer to Generate Name Filtered PDF=====================================
 
         app.post('/pdf/filtered', async (req, res) => {
-            // Get the filter criteria from the query parameter
-            const filter = {name: req.body.name};
-          
-            // Find the documents that match the filter criteria
-            const collection = db.collection('users');
-            const data = await collection.find(filter).toArray();
-          
-            // Generate the PDF as before
-            const html = `
-              <html>
-                <body>
-                  <h1>Data from the database</h1>
-                  <table>
+          // Get the filter criteria from the query parameter
+          const filter ={name: req.body.name};
+      
+          // Find the documents that match the filter criteria
+          const collection = db.collection('users');
+          const data = await collection.find(filter).toArray();
+      
+          // Generate the PDF as before
+          const html = `
+        <html>
+            <body>
+                <table>
                     <thead>
-                      <tr>
-                        <th>Name</th>
-                        <th>Number</th>
-                      </tr>
+                        <tr>
+                            
+                        </tr>
                     </thead>
                     <tbody>
-                      ${data.map(row => `
-                        <tr>
-                          <td>${row.name}</td>
-                          <td>${row.number}</td>
-                        </tr>
-                      `).join('')}
+                        ${data.map(row => `
+                            ${row.number.map((number,i) => `
+                                <tr>
+                                    ${i==0 ? `<td>${row.name}</td>` : `<td></td>`}
+                                </tr>
+                                <tr>
+                                    <td>${number}</td>
+                                </tr>
+                            `).join('')}
+                        `).join('')}
                     </tbody>
-                  </table>
-                </body>
-              </html>
-            `;
-            const browser = await puppeteer.launch({args: ['--no-sandbox']});
-
-            const page = await browser.newPage();
-            await page.setContent(html);
-            const buffer = await page.pdf({ format: 'A4' });
-            await browser.close();
-          
-            // Send the PDF to the client
-            res.setHeader('Content-Type', 'stickman/pdf');
-            res.setHeader('Content-Disposition', 'attachment; filename="stickman.pdf"');    
-            res.send(buffer);
-          });
-          
-
-
+                </table>
+            </body>
+        </html>
+    `;
+          const browser = await puppeteer.launch({args: ['--no-sandbox']});
+          const page = await browser.newPage();
+          await page.setContent(html);
+          const buffer = await page.pdf({ format: 'A4' });
+          await browser.close();
+      
+          // Send the PDF to the client
+          res.setHeader('Content-Type', 'application/pdf');
+          res.setHeader('Content-Disposition', `attachment; filename="${req.body.name}-${new Date().toLocaleDateString()}.pdf"`);
+  
+          res.send(buffer);
+      });
+      
 
         // Using Puppeteer for Admin's PDF generation==================================================================
 
         app.post('/pdf/admin', async (req, res) => {
 
             const collection = db.collection('users');
-            // const filter = { name: 'c' };
+            // const filter = {};
             // const data = await collection.find(filter).toArray();
             const data = await collection.find().toArray();
 
@@ -176,6 +180,69 @@ MongoClient.connect(db, { useNewUrlParser: true }, function (err, client) {
             res.send(buffer);
 
         });
+
+
+        //=========================Filter PDF by DATE===============================//
+        
+      //   app.post('/pdf/date', async (req, res) => {
+      //     let startDate = new Date(req.body.startDate);
+      //     let endDate = new Date(req.body.endDate);
+      //     startDate = startDate.toISOString();
+      //     endDate = endDate.toISOString();
+      //     // Get the filter criteria from the query parameters
+      //     const filter = { 
+      //       name: req.body.name, 
+      //       date: { 
+      //           $gte: new Date(req.body.startDate), 
+      //           $lte: new Date(req.body.endDate) 
+      //       }
+      //   };
+        
+      
+      //     // Find the documents that match the filter criteria
+      //     const collection = db.collection('users');
+      //     const data = await collection.find(filter).toArray();
+      
+      //     // Generate the PDF
+      //     const html = `
+      //         <html>
+      //             <body>
+      //                 <table>
+      //                     <thead>
+      //                         <tr>
+                                  
+      //                         </tr>
+      //                     </thead>
+      //                     <tbody>
+      //                         ${data.map(row => `
+      //                             ${row.number.map((number,i) => `
+      //                                 <tr>
+      //                                     ${i==0 ? `<td>${row.name}</td>` : `<td></td>`}
+      //                                 </tr>
+      //                                 <tr>
+      //                                     <td>${number}</td>
+      //                                 </tr>
+      //                             `).join('')}
+      //                         `).join('')}
+      //                     </tbody>
+      //                 </table>
+      //             </body>
+      //         </html>
+      //     `;
+      //     const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+      //     const page = await browser.newPage();
+      //     await page.setContent(html);
+      //     const buffer = await page.pdf({ format: 'A4' });
+      //     await browser.close();
+      
+      //     // Send the PDF to the client
+      //     res.setHeader('Content-Type', 'application/pdf');
+      //     res.setHeader('Content-Disposition', `attachment; filename="${req.body.name}-${new Date().toLocaleDateString()}.pdf"`);
+      //     res.send(buffer);
+      // });
+      
+
+        
 
 
     
